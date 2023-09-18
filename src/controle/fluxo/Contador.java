b5c2dc7de8f3efe4734da220628b43e16a1a85b7
Joao-Pereira013 <joaov.batalha013@hotmail.com>
2023-09-18 16:32:28 -0300

    first commit

---
+++ src/controle/fluxo/Contador.java
@@ -0,0 +1,44 @@
+package controle.fluxo;
+
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro: ");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro: ");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            // Chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println(exception.getMessage());
+        } finally {
+            // Certifique-se de fechar o scanner quando terminar de usá-lo
+            terminal.close();
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        // Validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior ou igual ao primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm;
+        // Realizar o for para imprimir os números com base na variável contagem
+        for (int i = 0; i <= contagem; i++) {
+            System.out.println(parametroUm);
+            parametroUm++;
+        }
+    }
+}
+
+class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
